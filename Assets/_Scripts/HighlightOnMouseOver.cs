ce715316e69d236bdedb650e9ba745ca9f0abe7f
CoClarkin <150698933+CoClarkin@users.noreply.github.com>
2023-12-04 18:28:15 +0000

    skybox and flashing ear
    ear doesnt flash yet

--- Assets/_Scripts/HighlightOnMouseOver.cs
+++ Assets/_Scripts/HighlightOnMouseOver.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 
 public class HighlightOnMouseOver : MonoBehaviour
 
-{
+    
+{   
+    public Material material1;
+    public Material material2;
+    public GameObject ear;
+    float duration = 2.0f;
+    Renderer rend;
     
     public Material EarSelected;
     public Material OriginalColor; 
 
-      // Start is called before the first frame update
     void Start()
+    //highlight ear on mouse over
     {
+        rend = ear.GetComponent<Renderer> ();
         OriginalColor = GetComponent<Renderer>().material;
     }
-        void OnMouseEnter()
+
+    void OnMouseEnter()
     {
         gameObject.GetComponent<Renderer>().material = EarSelected;
     }
+    
     void OnMouseExit()
     {
         gameObject.GetComponent<Renderer>().material = OriginalColor;
+    }
+    
+    void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        rend.material.Lerp(material1, material2, lerp);
     }
 }
