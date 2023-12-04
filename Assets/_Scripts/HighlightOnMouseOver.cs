ca538e6e0d3caa0344b0215f0d3fbef3bc76ac7f
CoClarkin <150698933+CoClarkin@users.noreply.github.com>
2023-12-04 19:24:21 +0000

    HighlightFlash
    new highlight animation

--- Assets/_Scripts/HighlightOnMouseOver.cs
+++ Assets/_Scripts/HighlightOnMouseOver.cs
@@ -8,19 +8,47 @@ public class HighlightOnMouseOver : MonoBehaviour
 {
     
     public Material EarSelected;
-    public Material OriginalColor; 
+    public Material OriginalColor;
+    public GameObject ear;
+    public Animator flickerAnimation;
+
+    Renderer rend;
+
+    float duration = 1f;
+    bool isFlicker;
 
       // Start is called before the first frame update
     void Start()
     {
-        OriginalColor = GetComponent<Renderer>().material;
+        //OriginalColor = GetComponent<Renderer>().material;
+        rend = ear.GetComponent<Renderer>();
+        
+        StartCoroutine("HighlightFlicker");
     }
         void OnMouseEnter()
     {
+        isFlicker = false;
+        flickerAnimation.SetBool("FlashOn",false);
         gameObject.GetComponent<Renderer>().material = EarSelected;
     }
     void OnMouseExit()
     {
         gameObject.GetComponent<Renderer>().material = OriginalColor;
     }
+
+    private void Update()
+    {
+        if (isFlicker)
+        {
+         flickerAnimation.SetBool("FlashOn",true);
+        }
+    }
+
+
+    IEnumerator HighlightFlicker()
+    {
+        yield return new WaitForSeconds(5.0f);
+        isFlicker = true;
+    }
+
 }
